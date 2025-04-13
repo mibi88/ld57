b6cade19083a1c12fa0ac6a87f1ad54fb1197101
mibi88 <mbcontact50@gmail.com>
2025-04-13 16:35:32 +0200

    Updated build file to support building for the web

--- build.sh
+++ build.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-cc src/*.c -Isrc -I../MibiEngine2/include ../MibiEngine2/build/MibiEngine2.a \
-   -lm -lEGL -lX11 -lGL -lpng -Wall -Wextra -Wpedantic -ansi -g -o main
+emscripten=false
+
+while getopts "e" flag; do
+    case "${flag}" in
+        e) emscripten=true ;;
+    esac
+done
+
+CC=cc
+BIN=main
+
+CFLAGS=(-Isrc -I../MibiEngine2/include ../MibiEngine2/build/MibiEngine2.a \
+        -lm -lEGL -lX11 -lGL -lpng -Wall -Wextra -Wpedantic -ansi -g)
+
+if $emscripten; then
+    echo "-- Using emscripten!"
+    CC=emcc
+    CFLAGS=$CFLAGS" -sUSE_ZLIB=1 --embed-file build/assets@/"
+    BIN=build/index.js
+    mkdir -p build
+    mkdir -p build/assets
+    cp -r shaders build/assets/shaders
+    cp -r assets build/assets/assets
+    cp ../MibiEngine2/data/index.html build
+fi
+
+$CC src/*.c ${CFLAGS[@]} -o $BIN
 
